3e644e89f1da858c7939a64f97b74612ee7f7844
Trajy <henrique.trajano.a@gmail.com>
2022-09-27 12:21:59 -0300

    renamed-directory

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api import viewsets
-
 from rest_framework import routers
 
 route = routers.DefaultRouter()
