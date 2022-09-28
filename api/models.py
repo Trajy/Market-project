112eae3e83b5b993dde29a668042954a3884144b
Trajy <henrique.trajano.a@gmail.com>
2022-09-27 22:13:36 -0300

    added-relashionship-between-order-and-item

--- api/models.py
+++ api/models.py
@@ -1,17 +1,22 @@
+from pyexpat import model
 from django.db import models
-
-class Item(models.Model):
-    id = models.AutoField(primary_key=True, editable=False)
-    name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
 
 class Customer(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True, editable=False)
 
+class Item(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
 class Order(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     code = models.CharField(max_length=50)
     data = models.DateField()
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, null=False)
+
+class ItemOrder(models.Model):
+    item = models.ForeignKey(Item, models.DO_NOTHING)
+    order = models.ForeignKey(Order, models.DO_NOTHING)
