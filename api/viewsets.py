a612fe80a14eaca5ff98564c086dd7ecf7f52e3f
Trajy <henrique.trajano.a@gmail.com>
2022-10-09 21:43:15 -0300

    created-business-rules-for-add-new-item-in-order

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from uuid import uuid4
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from api import models, serializers
@@ -8,12 +10,24 @@ class ItemViewSet(ModelViewSet):
 
 class OrderViewSet(ModelViewSet):
     serializer_class = serializers.OrderSerializer
-    queryset = models.Order.objects.all()
+    queryset = models.Order.objects.filter(status__exact='COMPRANDO').all()
+
 
 class ItemOrderViewSet(ModelViewSet):
     serializer_class = serializers.ItemOrderSerializer
     queryset = models.ItemOrder.objects.all()
 
+    def create(self, request):
+        item = models.Item.objects.filter(rfid__exact=request.data['rfid'])
+        if not(item.exists()):
+            return Response('Item nao cadastrado no sistema').status_code(404)
+        order = models.Order.objects.filter(status__icontains='COMPRANDO', customer__exact=request.data['customer'])
+        if not(order.exists()):
+           models.Order(code=uuid4(), data=datetime.now(), customer=request.data['customer'], status='COMPRANDO').save()
+           order = models.Order.objects.filter(status__icontains='COMPRANDO', customer__exact=request.data['customer'])
+        models.ItemOrder(item=item.id, order=order.id).save()
+        
+    
 class CustomerViewSet(ModelViewSet):
     serializer_class = serializers.CustomerSerializer
     queryset = models.Customer.objects.all()
