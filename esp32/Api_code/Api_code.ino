5a06c18cc46e9a7eff61c6e06b8b5d8121d6fbc1
Trajy <henrique.trajano.a@gmail.com>
2022-10-10 22:43:57 -0300

    implemented-patch-and-post-methods

--- esp32/Api_code/Api_code.ino
+++ esp32/Api_code/Api_code.ino
@@ -12,13 +12,30 @@ const char* password = "";
 HTTPClient http;
 SoftwareSerial rfid(5,4); // (RX, TX)
 
-void sendRequest(String body, String path) {
-    //chama a api de inicio de pedido
-    //verifica qual foi o ultimo pedido
+void prepararRequest(String path) {
     Serial.println("Call Api: " + API_URL + path);
     http.begin(API_URL + path);
     http.addHeader("Content-Type", "application/json");
+}
+
+void post(String body, String path) {
+    prepararRequest(path);
     int statusCode = http.POST(body);
+    if (statusCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String resposta = http.getString();
+        Serial.println(resposta);
+    }
+    else {
+        Serial.print("Error code: ");
+        Serial.println(statusCode);
+    }
+}
+
+void patch(String body, String path) {
+    prepararRequest(path);
+    int statusCode = http.PATCH(body);
     if (statusCode > 0) {
         Serial.print("HTTP Response code: ");
         Serial.println(httpResponseCode);
@@ -55,7 +72,7 @@ void setup() {
 }
 
 void loop() {
-    if(while (RFID.available() <= 0) {
+    if(RFID.available() <= 0) {
       return;
     }
     String tag = lerTag();
@@ -65,15 +82,17 @@ void loop() {
     if(tag == TAG_PEDIDO_FINALIZADO) {
         body = "{\"status\":\"FINALIZADO\"}";
         path = "/orders/"
+        patch(body, path);
     }
     else if(tag == TAG_PEDIDO_CANCELADO) {
       body = "{\"status\":\"CANCELADO\"}";
       path = "/orders/"
+      patch(body, path)
     }
     else {
         body = "{\"rfid\":\"" + tag + ",\"customer\":\"" + ID_ARDUINO + "\"}";
-        path = "/items-orders/"
+        path = "/items-orders/" + orderId + "/"
+        post(body, path)
     }
-    sendRequest(body, path);
     delay(1000);
 }
