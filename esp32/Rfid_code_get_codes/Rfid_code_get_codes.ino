9581f8ab776467cf531af722ec7ab62034d1eb6b
Trajy <henrique.trajano.a@gmail.com>
2022-10-12 21:36:19 -0300

    ajusts-in-get-tag-values-source-code

--- esp32/Rfid_code_get_codes/Rfid_code_get_codes.ino
+++ esp32/Rfid_code_get_codes/Rfid_code_get_codes.ino
@@ -1,22 +1,27 @@
 #include <SoftwareSerial.h>
-SoftwareSerial RFID(5,4); // RX and TX
+SoftwareSerial RFID(4,2); // RX and TX
 
 String text;
 
 void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);
+  
   Serial.begin(9600);
   RFID.begin(9600);
-  Serial.println("Aproxime a TAG ao Leitor."); 
+  Serial.println("Aproxime a TAG ao Leitor.");
+  digitalWrite(LED_BUILTIN, LOW);
 }
 char c;
 
 void loop()
 {
   while (RFID.available() > 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(5);
     c = RFID.read();
     text += c;
+    digitalWrite(LED_BUILTIN, LOW);
   }
   if (text.length() > 11)
   check();
