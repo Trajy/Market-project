c7fb664a3d0758d1711418042600361121f47e76
Trajy <henrique.trajano.a@gmail.com>
2022-10-12 21:37:44 -0300

    fix-bugs-in-main-code-to-request

--- esp32/Api_code/Api_code.ino
+++ esp32/Api_code/Api_code.ino
@@ -1,32 +1,41 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
-#define ID_ARDUINO 4
-#define TAG_PEDIDO_CANCELADO ""
-#define TAG_PEDIDO_FINALIZADO "";
-#define API_URL "https://market-api-tcc.herokuapp.com"
+#define ID_ARDUINO 1
+#define TAG_PEDIDO_CANCELADO String("3A007CDE0D")
+#define TAG_PEDIDO_FINALIZADO String("3900805A05")
+#define API_URL String("http://market-api-tcc.herokuapp.com")
 
 // Informe as credencias da sua rede wifi
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "Mortadela 2.4";
+const char* password = "7890048678900486";
+String pedidoId;
 HTTPClient http;
 SoftwareSerial rfid(5,4); // (RX, TX)
+DynamicJsonDocument doc(1024);
 
 void prepararRequest(String path) {
     Serial.println("Call Api: " + API_URL + path);
     http.begin(API_URL + path);
     http.addHeader("Content-Type", "application/json");
+    http.addHeader("Connection", "keep-alive");
 }
 
 void post(String body, String path) {
     prepararRequest(path);
+    http.addHeader("Content-Length", String(body.length()));
     int statusCode = http.POST(body);
     if (statusCode > 0) {
         Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
+        Serial.println(statusCode);
         String resposta = http.getString();
+        deserializeJson(doc, resposta);
+        JsonObject obj = doc.as<JsonObject>();
+        pedidoId = obj["order"].as<String>();
         Serial.println(resposta);
+        Serial.println(pedidoId);
     }
     else {
         Serial.print("Error code: ");
@@ -35,11 +44,12 @@ void post(String body, String path) {
 }
 
 void patch(String body, String path) {
+    http.addHeader("Content-Length", String(body.length()));
     prepararRequest(path);
     int statusCode = http.PATCH(body);
     if (statusCode > 0) {
         Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
+        Serial.println(statusCode);
         String resposta = http.getString();
         Serial.println(resposta);
     }
@@ -50,50 +60,53 @@ void patch(String body, String path) {
 }
 
 String lerTag() {
-    String tag = ""; 
-    while (RFID.available() > 0) {
-    delay(5);
-      tag += RFID.read();
+    String text = "";
+    while (rfid.available() > 0) {
+        delay(5);
+        text += (char)rfid.read();
     }
-    if (tag.length() > 11) {
-      return tag;
-    }
+    if (text.length() > 20)
+    return text.substring(1,11);
 }
 
 void setup() {
     pinMode (LED_BUILTIN, OUTPUT);
-    Serial.begin(115200);
+    Serial.begin(9600);
+    rfid.begin(9600);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN))
+        delay(100);
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     }
+    digitalWrite(LED_BUILTIN, LOW);
     Serial.println("Wifi conectado, IP address: ");
     Serial.println(WiFi.localIP());
 }
 
 void loop() {
-    if(RFID.available() <= 0) {
+    if(!rfid.available() > 0) {
       return;
     }
     String tag = lerTag();
-    Serial.println("Tag : " + tag);
     String body;
     String path;
+    digitalWrite(LED_BUILTIN, HIGH);
     if(tag == TAG_PEDIDO_FINALIZADO) {
         body = "{\"status\":\"FINALIZADO\"}";
-        path = "/orders/"
+        path = "/orders/" + pedidoId + "/";
         patch(body, path);
     }
     else if(tag == TAG_PEDIDO_CANCELADO) {
       body = "{\"status\":\"CANCELADO\"}";
-      path = "/orders/"
-      patch(body, path)
+      path = "/orders/"+ pedidoId + "/";
+      patch(body, path);
     }
     else {
-        body = "{\"rfid\":\"" + tag + ",\"customer\":\"" + ID_ARDUINO + "\"}";
-        path = "/items-orders/" + orderId + "/"
-        post(body, path)
+        body = "{\"rfid\":\"" + tag + "\",\"customer\":\"" + ID_ARDUINO + "\"}";
+        Serial.println("BODY: " + body);
+        path = "/items-orders/";
+        post(body, path);
     }
     delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
 }
